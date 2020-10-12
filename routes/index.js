d0913f6530a74b460ee4ff0a7081b5da8cd1bee1
Guy Haviv <theguyhaviv@gmail.com>
2020-10-12 20:04:58 +0300

    final game remarks added to landing page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express'),
 Bracket = require('../models/bracket'),
 router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('landingpage')
+router.get('/',async (req,res)=>{
+    const brackets = await Bracket.find({});
+    const noAdminBrackets = brackets.filter(bracket => !(bracket.isAdminBracket));
+    noAdminBrackets.sort((a,b)=>{
+        return b.score - a.score;
+    });
+    res.render('landingpage',{brackets : noAdminBrackets});
 })
 
 router.get('/leaderboard',async (req,res)=>{
